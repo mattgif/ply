8eb2aaef6199d406bc4b61b8c7bc4bb0bea54b46
Matt Gifford <mattgif@gmail.com>
2018-01-17 15:06:29 -0500

    splash mocked up

---
+++ public/js/index.js
@@ -0,0 +1,94 @@
+// modals 
+function modalJoinClick() {
+	// displays join modal on click
+	$('body').on('click', '.join', e => {
+		e.preventDefault();
+		modalOpen('.modal__join','#signup-email');
+	})
+}
+
+function modalLoginClick() {
+	$('html').on('click', '.login', e => {
+		e.preventDefault();
+		modalOpen('.modal__login', '#login-email');
+	})
+}
+
+function modalOpen(modalDialog,focusTarget) {
+	if ($('.modal__dialog').is(":visible")) {
+		$('.modal__dialog').hide();
+		$(modalDialog).show();
+	} else {
+		$('.js-modal-wrapper').css('top', 0);
+		$('.modal__overlay').fadeIn(400);		
+		$(modalDialog).slideDown(100);	
+	}
+	$(focusTarget).focus();
+	modalCloseListeners();		
+	modalKeepTabFocus();
+}
+
+function modalClose() {
+	$('.modal__overlay, .modal__dialog').fadeOut(200);
+	setTimeout(function() {
+		$('.js-modal-wrapper').css('top', '-100%')
+	},200);
+	// turn off listeners used to keep focus in modal
+	$('.join, .login').off("keydown");
+	$('.js-modal-wrapper, .js-modal-dialog').off("click");
+	$('.js-modal-close').off("click")
+}
+
+function modalCloseClick() {
+	$('.js-modal-close').click(e => {
+		e.preventDefault;
+		modalClose();
+	})
+}
+
+function modalEscapePress() {
+	$('body').keydown(e => {
+			// hide modal on escape press
+			if (e.keyCode==27){
+				modalClose();
+			}			
+		})
+}
+
+function modalOutsideClick() {
+	$('.js-modal-dialog').click(e => {
+		e.stopPropagation();		
+	})
+	$('.js-modal-wrapper').click(() => {
+		modalClose();
+	})
+}
+
+function modalKeepTabFocus() {
+	// prevents tab from leaving modal by going back to top focusable element in dialog 
+	// after last is reached
+	$('.join, .login').keydown(e => {
+		if (e.keyCode === 9) {
+			e.preventDefault();				
+			$('.js-modal-close').focus();
+		}
+	})	
+}
+
+function modalCloseListeners() {
+	modalCloseClick();
+	modalEscapePress();
+	modalOutsideClick();
+}
+
+function modalOpenListeners() {
+	modalJoinClick();
+	modalLoginClick();
+}
+
+// core
+function pageHandler() {	
+	modalOpenListeners();	
+}
+
+$(pageHandler);
---
+++ public/js/mock.js
@@ -0,0 +1,48 @@
+const MOCK_SPACE_LOCATIONS = {
+	"locations": [
+		{
+			"id": "111111",
+			"spaceID":"g11111120002",
+			"title": "Barn with power and water",
+			"owner": "userID11111111",
+			"location": "geo info???",
+			"coverImage" : "barn.jpg",
+			"unavailability": {								
+				2018: {
+					1: {
+						29: [17,18,19,20,21,22,23],
+						30: [9, 10, 11, 14, 15, 16],
+					},
+					2: {
+						1: [9, 10, 11, 12, 13, 14, 15, 16],
+						2: [9, 10, 11, 12, 13, 14, 15, 16],
+						3: [9, 10, 11, 12, 13, 14, 15, 16],
+					}
+				}
+			}			
+		},		
+		{
+			"id": "222222",
+			"spaceID":"g22222220002",
+			"title": "One half of two car garage",
+			"owner": "userID222222222",
+			"location": "geo info???",
+			"coverImage" : "garage.jpg",
+			"availability": {
+				2018: {
+					1: {
+						29: [14, 15, 16, 17, 18],
+						30: [9, 10, 11, 14, 15, 16],
+					},
+					2: {
+						1: [9, 10, 11, 12, 13, 14, 15, 16],
+						2: [9, 10, 11, 12, 13, 14, 15, 16],
+						3: [9, 10, 11, 12, 13, 14, 15, 16],
+					}
+				}
+			}			
+		},
+
+	]
+}
+
